d58d33ae8af318ef27dce4da490c321f0fd7b570
Rakesh kundan <rakeshkundankps@gmail.com>
2023-01-08 13:37:03 +0530

    initial version corrected image

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var rand1=Math.random();
 rand1=Math.floor(rand1*6)+1;
 var rand2=Math.random();
 rand2=Math.floor(rand2*6)+1;
-document.querySelector(".img1").setAttribute("src",'/images/dice'+rand1+'.png');
+document.querySelector(".img1").setAttribute("src",'images/dice'+rand1+'.png');
 
-document.querySelector(".img2").setAttribute("src","/images/dice"+rand2+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
 document.querySelector(".img1").attributes;
 
 
